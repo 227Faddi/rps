fcd752bb8f79af373cdd903bedbaf895b3a500b9
227Faddi <falukhouma27@gmail.com>
2025-03-31 15:23:51 -0400

    refactor: add f strings

--- rps.py
+++ rps.py
@@ -3,6 +3,7 @@ import random
 from enum import Enum
 
 def rps():
+
     game_count = 0
     player_wins = 0
     computer_wins = 0
@@ -26,27 +27,36 @@ def rps():
 
         computer = int(computer_choice)
 
-        print('\nYou chose ' + str(RPS(player)).replace('RPS.', '') + '.')
-        print('Computer chose ' + str(RPS(computer)).replace('RPS.', '') + '.\n')
+        print(f'\nYou chose {str(RPS(player)).replace('RPS.', '')}.')
+        print(f'Computer chose {str(RPS(computer)).replace('RPS.', '')}.\n')
 
         def game_result(player, computer):
+            nonlocal player_wins
+            nonlocal computer_wins
             if player == 1 and computer == 3:
+                player_wins += 1
                 return '🥰 You win!'
             elif player == 2 and computer == 1:
+                player_wins += 1
                 return '🥰 You win!'
             elif player == 3 and computer == 2:
+                player_wins += 1
                 return '🥰 You win!'
             elif player == computer:
                 return '🫡 Tie Game!'
             else:
+                computer_wins += 1
                 return '🐍 Computer wins!'
             
         print(game_result(player, computer))
 
-        global game_count
+        nonlocal game_count
         game_count += 1
 
-        print('\nGame count:' + str(game_count))
+        print(f'\nGame count: {str(game_count)}')
+        print(f'\nPlayer wins: {str(player_wins)}')
+        print(f'\nComputer wins: {str(computer_wins)}')
+
         
         print()
         while True:
@@ -63,5 +73,9 @@ def rps():
             print('\nThanks for playing!!\n\n🎉🎉🎉\n')
             sys.exit('Bye 👋, see you soon!')
 
+    return game()
 
-rps()
+
+play = rps()
+
+play()
